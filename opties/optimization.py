0786bbe8d69a3648f57e17cd0d10cac55e0da8a7
KathiEsterl <katharina.esterl@uni-flensburg.de>
2023-10-19 16:11:33 +0200

    Correct chp-constraints, apply black

--- opties/optimization.py
+++ opties/optimization.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2023 
+# Copyright 2023
 # Europa-Universität Flensburg,
 # Centre for Sustainable Energy Systems,
 # FossilExit Research Group
@@ -32,18 +32,17 @@ __copyright__ = (
 __license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
 __author__ = "KathiEsterl, mohsenmansouri"
 
+
 def optimization(network, args):
-    
-    method = args['method']
+    method = args["method"]
     path = args["csv_export"]
-    
+
     # snapshots
-    start = args["start_snapshot"]-1
+    start = args["start_snapshot"] - 1
     end = args["end_snapshot"]
     snapshots = network.snapshots[start:end]
-    
+
     if network.lines.s_nom_extendable.any():
-        
         # s_nom_pre = s_nom_opt of previous iteration
         l_snom_pre = network.lines.s_nom.copy()
 
@@ -84,73 +83,77 @@ def optimization(network, args):
     else:
         run_lopf(network, args, Constraints().extra_functionalities)
 
+
 def run_lopf(network, args, extra_functionality):
-    
     x = time.time()
-    
+
     # snapshots
-    start = args["start_snapshot"]-1
+    start = args["start_snapshot"] - 1
     end = args["end_snapshot"]
-    
-    #network.optimize.create_model()
-    
+
+    # network.optimize.create_model()
+
     network.lopf(
-            snapshots=network.snapshots[start:end],
-            pyomo=args["method"]["pyomo"],
-            solver_name=args["solver_name"],
-            solver_options=args["solver_options"],
-            extra_functionality=extra_functionality
-        )
+        snapshots=network.snapshots[start:end],
+        pyomo=args["method"]["pyomo"],
+        solver_name=args["solver_name"],
+        solver_options=args["solver_options"],
+        extra_functionality=extra_functionality,
+    )
 
     # TODO: Exception ?
     # TODO: pyomo vs. ohne pyomo unterscheiden ?
-                
+
     y = time.time()
     z = (y - x) / 60
 
     print("Time for LOPF [min]:", round(z, 2))
-    
+
     network.export_to_csv_folder(args["csv_export"])
 
+
 def add_chp_constraints_nmp(n, sns):
-    
     # Konstanten
-    c_m = 0.75 # backpressure limit
-    c_v = 0.15 # marginal loss for each additional generation of heat
+    c_m = 0.75  # backpressure limit
+    c_v = 0.15  # marginal loss for each additional generation of heat
 
     # KWK: elektrische und Wärme-Links
-    electric_bool = n.links.carrier == 'KWK_AC'
-    heat_bool = n.links.carrier == 'KWK_heat'
+    electric_bool = n.links.carrier == "KWK_AC"
+    heat_bool = n.links.carrier == "KWK_heat"
     electric_links = n.links.index[electric_bool]
     heat_links = n.links.index[heat_bool]
 
     # Effizienzen Wärme-Links
-    n.links.loc[heat_links, "efficiency"] = (n.links.loc[electric_links, "efficiency"] / c_v).values.mean()
-    
-    # KWK-Gasknoten
-    ch4_nodes_with_chp = n.buses.loc[n.links[n.links.index.str.startswith('KWK')].bus0].index.unique()
+    n.links.loc[heat_links, "efficiency"] = (
+        n.links.loc[electric_links, "efficiency"] / c_v
+    ).values.mean()
 
-    # Define the power variables for links and generators
-    #link_p = get_var(n, "Link", "p")
-    #gen_p = get_var(n, "Generator", "p")
+    # KWK-Gasknoten
+    ch4_nodes_with_chp = n.buses.loc[
+        n.links[n.links.index.str.startswith("KWK")].bus0
+    ].index.unique()
 
     for i in ch4_nodes_with_chp:
-        
         # Links an ausgewähltem Knoten
-        elec = n.links[(n.links.carrier=='KWK_AC') & (n.links.bus0 == i)].index
-        heat = n.links[(n.links.carrier=='KWK_W') & (n.links.bus0 == i)].index
-        
+        elec = n.links[(n.links.carrier == "KWK_AC") & (n.links.bus0 == i)].index
+        heat = n.links[(n.links.carrier == "KWK_heat") & (n.links.bus0 == i)].index
+
         # Leistung an Links (Optimierungsvariable)
         link_p = get_var(n, "Link", "p")
 
-        # Constraints ensuring that heat and electricity production don't exceed limits based on the biogas input
-        lhs_1 = sum(c_m * n.links.at[h_chp, "efficiency"] * link_p[h_chp] for h_chp in heat)
+        # Verhältnis aus Strom- und Wärmeoutput
+
+        lhs_1 = sum(
+            c_m * n.links.at[h_chp, "efficiency"] * link_p[h_chp] for h_chp in heat
+        )
         lhs_2 = sum(n.links.at[e_chp, "efficiency"] * link_p[e_chp] for e_chp in elec)
-        
-        lhs = linexpr((1, lhs_1), (-c_v, lhs_2))
+
+        lhs = linexpr((1, lhs_1), (-1, lhs_2))
         define_constraints(n, lhs, "<=", 0, "chplink_" + str(i), "backpressure")
 
+        # Constraints zur Begrenzung des Biogaseinsatzes für Strom- und Wärmeoutput
         # top_iso_fuel_line
+
         lhs, *ax = linexpr(
             (1, sum(link_p[h_chp] for h_chp in heat)),
             (1, sum(link_p[h_e] for h_e in elec)),
@@ -166,9 +169,12 @@ def add_chp_constraints_nmp(n, sns):
             "top_iso_fuel_line_fix",
             axes=ax,
         )
-    
-class Constraints:
 
+
+# TODO: pyomo-constraints
+
+
+class Constraints:
     def extra_functionalities(self, network, snapshots):
         """Add constraints to pypsa-model using extra-functionality.
 
