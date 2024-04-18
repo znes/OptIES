6179666bb8a154d57388f65cb2114d2dc142bb6c
KathiEsterl <katharina.esterl@uni-flensburg.de>
2024-04-18 10:46:32 +0200

    Adaptions to measured time series for one full year

--- opties/data.py
+++ opties/data.py
@@ -22,6 +22,7 @@ This file contains the functions to import the corresponding data and build
 a PyPSA network container.
 """
 
+import numpy as np
 import pandas as pd
 import geopandas as gpd
 import shapely
@@ -37,7 +38,10 @@ __license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
 __author__ = "KathiEsterl"
 
 
-def import_data(path="data/"):
+def import_data(args):
+    
+    path=args["path"]  
+    
     buses = pd.read_csv(path + "buses.csv").set_index("name")
     if "geometry" in buses.columns:
         buses["geometry"] = buses["geometry"].apply(shapely.wkt.loads)
@@ -50,33 +54,70 @@ def import_data(path="data/"):
     links = pd.read_csv(path + "links.csv").set_index("name")
     loads = pd.read_csv(path + "loads.csv").set_index("name")
 
-    loads = loads.drop(["LS1", "LS2"])
-
     return buses, lines, generators, storage_units, stores, links, loads
 
 
-def import_timeseries(path="data/timeseries/", use_real_data=False):
+def import_timeseries(args):
+    
+    path=args["path"]+"timeseries/"
+    use_real_data=args["use_real_data"]
+    temporal=args["temporal_resolution"]
+    
     if use_real_data:
+        
+        # anpassen der network snapshots sowie der synthetischen Zeitreihen
+        # entsprechend des Zeitabschnitts der Messungen (Febraur 2023 - Februar 2024)
+        # und der der/des Nutzer*in gewählten zeitlichen Auflösung
+        
         el_loads = pd.read_csv(path + "el_load_real.csv").set_index("time")
-        el_loads.index = pd.date_range("2023-02-15 00:00", "2023-11-07 10:00", freq="H")
+        el_loads.index = pd.date_range("2023-02-14 00:00", "2024-02-13 23:55", freq="5min")
+        
+        if temporal == '5min':
+            res = 12
+            index=pd.date_range("2023-02-14 00:00", "2024-02-13 23:55", freq="5min")
+        elif temporal == '15min':
+            res = 4
+            index=pd.date_range("2023-02-14 00:00", "2024-02-13 23:45", freq="15min")
+            eloads = el_loads.copy()
+            el_loads = pd.DataFrame(index=index)
+            el_loads = eloads.groupby(np.arange(len(eloads))//3).mean()
+            el_loads = el_loads.set_index(index)
+        elif temporal == 'hourly':
+            res = 1
+            index=pd.date_range("2023-02-14 00:00", "2024-02-13 23:00", freq="1H")
+            eloads = el_loads.copy()
+            el_loads = pd.DataFrame(index=index)
+            el_loads = eloads.groupby(np.arange(len(eloads))//12).mean()
+            el_loads = el_loads.set_index(index)
+        
+        hload = pd.read_csv(path + "heat_load_synth.csv").set_index("time")
+        hload = hload.append(hload[:1056], ignore_index=True)
+        hload = hload.drop(hload.index[:1056])
+        heat_load = pd.DataFrame(index=index)
+        heat_load['WL'] = hload['WL'].values.repeat(res)
+        heat_load['EV_W'] = hload['EV_W'].values.repeat(res)
 
-        heat_load = pd.read_csv(path + "heat_load_synth.csv").set_index("time")[
-            1080:7451
-        ]
-        heat_load.index = pd.date_range(
-            "2023-02-15 00:00", "2023-11-07 10:00", freq="H"
-        )
+        gload = pd.read_csv(path + "gas_load_synth.csv").set_index("time")
+        gload = gload.append(gload[:1056], ignore_index=True)
+        gload = gload.drop(gload.index[:1056])
+        gas_load = pd.DataFrame(index=index)
+        gas_load['SAT'] = gload['SAT'].values.repeat(res)
 
-        gas_load = pd.read_csv(path + "gas_load.csv").set_index("time")[1080:7451]
-        gas_load.index = pd.date_range("2023-02-15 00:00", "2023-11-07 10:00", freq="H")
-
-        pv = pd.read_csv(path + "pv_timeseries.csv")[1080:7451]
-        pv = pd.Series(
-            pv["p_max_pu"].values,
-            index=pd.date_range("2023-02-15 00:00", "2023-11-07 10:00", freq="H"),
-        )
-
+        pvgen = pd.read_csv(path + "pot_pv_timeseries_synth.csv")
+        pvgen = pvgen.append(pvgen[:1056], ignore_index=True)
+        pvgen = pvgen.drop(pvgen.index[:1056])
+        pv = pd.Series(data = pvgen['p_max_pu'].values.repeat(res), index=index)
+            
     else:
+        
+        if temporal != 'hourly':
+            print(' ')
+            print('Bei Verwendung der synthetischen Daten ist eine stündliche Auflösung vorgesehen.')
+            print(' ')
+        
+        # bei Verwendung synthetischer Zeitreihen
+        # in fester stündlicher Auflösung für 2019
+        
         el_loads = pd.read_csv(path + "el_load_synth.csv").set_index("time")
         el_loads.index = pd.date_range("2019-01-01 00:00", "2019-12-31 23:00", freq="H")
 
@@ -85,10 +126,10 @@ def import_timeseries(path="data/timeseries/", use_real_data=False):
             "2019-01-01 00:00", "2019-12-31 23:00", freq="H"
         )
 
-        gas_load = pd.read_csv(path + "gas_load.csv").set_index("time")
+        gas_load = pd.read_csv(path + "gas_load_synth.csv").set_index("time")
         gas_load.index = pd.date_range("2019-01-01 00:00", "2019-12-31 23:00", freq="H")
 
-        pv = pd.read_csv(path + "pv_timeseries.csv")
+        pv = pd.read_csv(path + "pot_pv_timeseries_synth.csv")
         pv = pd.Series(
             pv["p_max_pu"].values,
             index=pd.date_range("2019-01-01 00:00", "2019-12-31 23:00", freq="H"),
@@ -109,14 +150,32 @@ def create_pypsa_network(
     heat_load,
     gas_load,
     pv,
-    use_real_data=False,
+    args,
 ):
     network = pypsa.Network()
+    
+    use_real_data=args["use_real_data"]
+    temporal=args["temporal_resolution"]
 
     if use_real_data:
-        network.set_snapshots(
-            pd.date_range("2023-02-15 00:00", "2023-11-07 10:00", freq="H")
-        )
+        
+        if temporal == '5min':
+        
+            network.set_snapshots(
+                pd.date_range("2023-02-14 00:00", "2024-02-13 23:55", freq="5min")
+            )
+            
+        elif temporal == '15min':
+        
+            network.set_snapshots(
+                pd.date_range("2023-02-14 00:00", "2024-02-13 23:45", freq="15min")
+            )
+            
+        else:
+        
+            network.set_snapshots(
+                pd.date_range("2023-02-14 00:00", "2024-02-13 23:00", freq="H")
+            )
 
     else:
         network.set_snapshots(
@@ -285,4 +344,203 @@ def create_pypsa_network(
                 p_set=gas_load[load.name],
             )
 
+    return network
+
+def calc_flex_potentials(p, delta_t, s_util, s_dec, s_inc, s_flex):
+    
+    # nach Heitkoetter et. al (doi:https://doi.org/10.1016/j.adapen.2020.100001)
+    
+    l = p * s_flex # scheduled load
+    energy = l.sum() # for max capacity
+    cap = (energy * s_flex) / (8760 * s_util) # max capacity
+    pmax = cap * s_inc - l
+    pmax[pmax < 0] = 0
+    pmin = -(l - cap * s_dec)
+    pmin[pmin > 0] = 0
+    emax = l.copy()
+    emin = l.copy() 
+    
+    # Berechnung der Potentiale
+    
+    for col in l.columns:
+        for i in range(len(l[col])):
+            if i + delta_t > len(l[col])-1:
+                emax.at[emax.index[i], col] = l.loc[l.index[i:]][col].sum() + l.loc[l.index[:delta_t-(len(l[col])-i)]][col].sum()
+            else:
+                emax.at[emax.index[i], col] = l.loc[l.index[i]:l.index[i+delta_t]][col].sum()
+            if i - delta_t < 0:
+                emin.at[emin.index[i], col] = -1 * (l.loc[l.index[:i]][col].sum() + l.loc[l.index[len(l[col])-delta_t+i:]][col].sum())
+            else:
+                emin.at[emin.index[i], col] = -1 * l.loc[l.index[i-delta_t]:l.index[i]][col].sum()
+                
+    pnom = pd.DataFrame({'pmax': pmax.max(), 'pmin': abs(pmin.min())}, index=pmax.columns)
+    pnom = pnom.max(axis=1)
+    enom = pd.DataFrame({'emax': emax.max(), 'emin': abs(emin.min())}, index=emax.columns)
+    enom = enom.max(axis=1)
+        
+    return pmax, pmin, emax, emin, pnom, enom
+
+
+def emob_potentials(network):
+    
+    # Implementierung nach Heitkoetter et. al (doi:https://doi.org/10.1016/j.adapen.2020.100001)
+
+    ls = network.loads.index[network.loads.index.str.startswith('LS')]
+    p = network.loads_t.p_set[ls]
+    
+    ## Parametrisierung nach Heitkoetter et. al (doi:https://doi.org/10.1016/j.adapen.2020.100001)
+    pmax, pmin, emax, emin, pnom, enom = calc_flex_potentials(p, 5, 0.07, 0, 0.25, 1)
+    pmax[pmax<0.00001] = 0 # vernachlässigbare Potentiale (< durch 0 ersetzen für bessere Lösbarkeit
+    pmin[pmin>-0.00001] = 0
+    
+    pmaxpu = pmax / pnom
+    pminpu = pmin / pnom
+    emaxpu = emax / enom
+    eminpu = emin / enom
+    
+    for ev in ls:
+        
+        network.add(
+            "Bus",
+            name=ev+'_flex',
+            carrier='AC',
+            v_nom=0.4,
+        )
+        
+        network.add(
+            "Link",
+            name=ev+'_flex',
+            carrier='AC',
+            bus0=ev,
+            bus1=ev+'_flex',
+            p_nom_extendable=False,
+            p_nom=pnom[ev],
+            p_min_pu=pminpu[ev],
+            p_max_pu=pmaxpu[ev],
+            efficiency=1,
+            marginal_cost=0,
+            capital_cost=0,
+        )
+        
+        network.add(
+            "Store",
+            name=ev+'_flex',
+            carrier='AC',
+            bus=ev+'_flex',
+            e_nom_extendable=False,
+            e_nom=enom[ev],
+            e_min_pu=eminpu[ev],
+            e_max_pu=emaxpu[ev],
+            standing_loss=0,
+            e_cyclic=True,
+            marginal_cost=0,
+            capital_cost=0,
+        )  
+
+
+def dsm_potentials(network):
+    
+    # Implementierung nach Heitkoetter et. al (doi:https://doi.org/10.1016/j.adapen.2020.100001)
+    
+    # 1) AN1: landwirtschaftlicher Betrieb
+    
+    # 2) weitere AN (normale Haushalte)
+    
+    an = network.loads.index[network.loads.index.str.startswith('AN')]
+    #an = an.drop('AN1')
+        
+    ## zeitabhängige Potentiale verschiedener Anwendungen berechnen
+    ## Parametrisierung nach Heitkoetter et. al (doi:https://doi.org/10.1016/j.adapen.2020.100001)
+    
+    p = network.loads_t.p_set[an]
+    # Wasch- und Trocknungsvorgänge
+    p1 = p * 0.09
+    pmax1, pmin1, emax1, emin1, pnom1, enom1 = calc_flex_potentials(p1, 6, 0.01, 0.0025, 0.025, 0.4)
+    # Kühlprozesse
+    p2 = p * 0.17
+    pmax2, pmin2, emax2, emin2, pnom2, enom2 = calc_flex_potentials(p2, 2, 0.33, 0, 1, 0.4)
+    
+    ## zeitabhängige Potentiale pro AN aufaddieren und für Komponenten vorbereiten 
+    
+    pmax = pmax1 + pmax2
+    pmin = pmin1 + pmin2
+    emax = emax1 + emax2
+    emin = emin1 + emin2
+    pmax[pmax<0.00001] = 0 # vernachlässigbare Potentiale durch 0 ersetzen für bessere Lösbarkeit
+    pmin[pmin>-0.00001] = 0
+    pnom = pd.DataFrame({'pmax': pmax.max(), 'pmin': abs(pmin.min())}, index=pmax.columns)
+    pnom = pnom.max(axis=1)
+    enom = pd.DataFrame({'emax': emax.max(), 'emin': abs(emin.min())}, index=emax.columns)
+    enom = enom.max(axis=1)
+    pmaxpu = pmax / pnom
+    pminpu = pmin / pnom
+    emaxpu = emax / enom
+    eminpu = emin / enom
+    
+    ## Komponenten mit zeitabhängigen Potentialen hinzufügen
+    
+    for house in an:
+        
+        network.add(
+            "Bus",
+            name=house+'_DSM',
+            carrier='AC',
+            v_nom=0.4,
+        )
+        
+        network.add(
+            "Link",
+            name=house+'_dsm',
+            carrier='AC',
+            bus0=house,
+            bus1=house+'_DSM',
+            p_nom_extendable=False,
+            p_nom=pnom[house],
+            p_min_pu=pminpu[house],
+            p_max_pu=pmaxpu[house],
+            efficiency=1,
+            marginal_cost=0,
+            capital_cost=0,
+        )
+        
+        network.add(
+            "Store",
+            name=house+'_dsm',
+            carrier='AC',
+            bus=house+'_DSM',
+            e_nom_extendable=False,
+            e_nom=enom[house],
+            e_min_pu=eminpu[house],
+            e_max_pu=emaxpu[house],
+            standing_loss=0,
+            e_cyclic=True,
+            marginal_cost=0,
+            capital_cost=0,
+        )   
+        
+
+def adapt_settings(network, args):
+    
+    # Flexibilitätspotentiale
+    
+    if 'emob' in args["flexible_components"]:
+        emob_potentials(network)
+        
+    if 'dsm' in args["flexible_components"]:
+        dsm_potentials(network)
+        
+    # ausbaubare Komponenten
+    
+    if 'el_lines' in args["extendable_components"]:
+        network.lines.s_nom_extendable = True
+        
+    if 'pv' in args["extendable_components"]:
+        network.generators[network.generators.carrier=='PV'].p_nom_extendable = True
+        
+    if 'battery' in args["extendable_components"]:
+        network.storage_units.at['BSp', 'p_nom_extendable'] = True
+    
+    if 'heat-store' in args["extendable_components"]:
+        network.stores.at['WSp', 'e_nom_extendable'] = True
+    
     return network
